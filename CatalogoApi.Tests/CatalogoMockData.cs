9fdc8850a326af58aa828fc6a32fe68e55afba7e
Rafael Naville <rafael.nc2@gmail.com>
2022-07-25 08:04:06 -0300

    Add tests

---
+++ CatalogoApi.Tests/CatalogoMockData.cs
@@ -0,0 +1,31 @@
+﻿using CatalogoApi.Context;
+using CatalogoApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CatalogoApi.Tests;
+
+public class CatalogoMockData
+{
+    public static async Task CreateCategories(CatalogoApiApplication application, bool criar)
+    {
+        using (var scope = application.Services.CreateScope())
+        {
+            var provider = scope.ServiceProvider;
+            using (var catalogoDbContext = provider.GetRequiredService<AppDbContext>())
+            {
+                await catalogoDbContext.Database.EnsureCreatedAsync();
+
+                if (criar)
+                {
+                    await catalogoDbContext.Categorias.AddAsync(new Categoria
+                    { Nome = "Categoria 1", Descricao = "Descricao Categoria 1" });
+
+                    await catalogoDbContext.Categorias.AddAsync(new Categoria
+                    { Nome = "Categoria 2", Descricao = "Descricao Categoria 2" });
+
+                    await catalogoDbContext.SaveChangesAsync();
+                }
+            }
+        }
+    }
+}
